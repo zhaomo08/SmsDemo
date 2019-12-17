c00ee2224f2134716f510979f4365c31a43ce200
jiacheng <1598575883@qq.com>
2019-12-17 15:21:31 +0800

    测试github集成idea

---
+++ src/main/java/com/offcn/SendSms.java
@@ -0,0 +1,45 @@
+package com.offcn;
+
+/**
+ * @author zjc
+ * @create 2019--12--12--16:49
+ */
+import com.aliyuncs.CommonRequest;
+import com.aliyuncs.CommonResponse;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.exceptions.ServerException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+
+
+public class SendSms {
+    public static void main(String[] args) {
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI4FmCZManL1RTJ8Yg8pYe", "bTiXnYIm9soPVDMza8hmBgr7Pp5IH9");
+        IAcsClient client = new DefaultAcsClient(profile);
+
+        CommonRequest request = new CommonRequest();
+        request.setMethod(MethodType.POST);
+        request.setDomain("dysmsapi.aliyuncs.com");
+        request.setVersion("2017-05-25");
+        request.setAction("SendSms");
+        request.putQueryParameter("RegionId", "cn-hangzhou");
+        //接收手机号
+        request.putQueryParameter("PhoneNumbers", "17629299160");
+        //模板名称
+        request.putQueryParameter("SignName", "优乐轩平台");
+        //模板编号
+        request.putQueryParameter("TemplateCode", "SMS_180058400");
+        //验证码值
+        request.putQueryParameter("TemplateParam", "{\"code\":\"1997082366\"}");
+        try {
+            CommonResponse response = client.getCommonResponse(request);
+            System.out.println(response.getData());
+        } catch (ServerException e) {
+            e.printStackTrace();
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+    }
+}
